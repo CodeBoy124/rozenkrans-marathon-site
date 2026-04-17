a87888b9ef40e278effcd019934e9c88babc157b
CodeBoy124 <koen7maart@gmail.com>
2026-04-17 12:10:43 +0200

    chore: small corrections for the astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import pageconfig from "./src/pageconfig.json";
 
 import svelte from "@astrojs/svelte";
 
@@ -8,6 +9,13 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: pageconfig.sitelocatie,
+
+  server: {
+    host: true,
+    port: 3000,
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
