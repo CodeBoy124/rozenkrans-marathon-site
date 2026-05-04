ca3e1e055c4b83cfd676abe7876c4a113c71bf12
CodeBoy124 <koen7maart@gmail.com>
2026-05-04 12:54:22 +0200

    feat: added skip to main link

--- astro.config.ts
+++ astro.config.ts
@@ -7,10 +7,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000,
-  },
+  // server: {
+  //   host: true,
+  //   port: 3000,
+  // },
 
   vite: {
     plugins: [tailwindcss()],
